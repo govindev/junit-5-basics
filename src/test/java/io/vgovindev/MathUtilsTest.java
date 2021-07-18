b3191815ac2648d7c7d2a26b61310139003b5959
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-07-18 23:10:58 +0530

    Test life cycle hooks - beforeeach

--- src/test/java/io/vgovindev/MathUtilsTest.java
+++ src/test/java/io/vgovindev/MathUtilsTest.java
@@ -2,13 +2,19 @@ package io.vgovindev;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
+	MathUtils mathUtils;
+
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
 
 	@Test
 	void testAdd() {
-		MathUtils mathUtils = new MathUtils();
 		int expected = 2;
 		int actual = mathUtils.add(1, 1);
 		assertEquals(expected, actual, "The add method should add two numbers");
@@ -17,14 +23,11 @@ class MathUtilsTest {
 
 	@Test
 	void testDivide() {
-		MathUtils mathUtils = new MathUtils();
-		// assertThrows(NullPointerException.class, () -> mathUtils.divide(1, 0), "Divide by zero should throw");
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "divide by zero should throw");
 	}
 
 	@Test
 	void testComputeCircleAre() {
-		MathUtils mathUtils = new MathUtils();
 		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
 
 	}

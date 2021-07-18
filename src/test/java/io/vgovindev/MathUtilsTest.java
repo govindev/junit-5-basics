97a4b5d9682b915f0b137eb1f7b62b15e499a7a8
Vurukuti Govinda Raju Yadav <vgovindev@gmail.com>
2021-07-18 22:22:31 +0530

    My First test

---
+++ src/test/java/io/vgovindev/MathUtilsTest.java
@@ -0,0 +1,14 @@
+package io.vgovindev;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MathUtilsTest {
+
+	@Test
+	void test() {
+		System.out.println("The test ran");
+	}
+
+}
